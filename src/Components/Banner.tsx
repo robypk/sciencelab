ad88c2fbc9224847458e75a88abba7a23f11f02e
ROBY P K <robypk93@gmail.com>
2023-12-20 01:27:30 +0530

    just update

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LabBanner from "../images/LabBanner.jpg";
+import LabBanner from "../../public/images/LabBanner.jpg";
 
 interface BannerProps {
   // props definition
@@ -11,10 +11,10 @@ const Banner: React.FC<BannerProps> = ({}) => {
       <div
         className=" relative bg-cover bg-center h-72"
         style={{
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url(${LabBanner})`,
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url("../images/LabBanner.jpg")`,
         }}
       >
-        <div className="absolute bottom-10 left-10 text-5xl text-slate-800">
+        <div className="absolute bottom-10 left-10 text-5xl text-orange-300">
           {" "}
           INFRAST LABORATORIES{" "}
         </div>
