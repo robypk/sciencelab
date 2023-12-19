6bdc2930517002fae6cdf9aeb782cd5b38676592
ROBY P K <robypk93@gmail.com>
2023-12-19 16:50:54 +0530

    setupNextConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { output: 'export',
  
+
 // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
 // trailingSlash: true,
 
@@ -9,6 +10,7 @@ const nextConfig = { output: 'export',
 
 // Optional: Change the output directory `out` -> `dist`
 // distDir: 'dist',
+
 }
 
 module.exports = nextConfig
