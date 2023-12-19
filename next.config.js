74f9ec13cbea8e8dd84665d183691d28d1202b83
ROBY P K <robypk93@gmail.com>
2023-12-19 17:03:44 +0530

    setupNextConfignewneww

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export',
- 
-
-// Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-// trailingSlash: true,
-
-// Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-// skipTrailingSlashRedirect: true,
-
-// Optional: Change the output directory `out` -> `dist`
-// distDir: 'dist',
-exportPathMap: async function () {
-    return {
-      '/': { page: '/' },
-      // Add other pages as needed
-    };
-  },
-
+const nextConfig = { trailingSlash: true,
+    output: 'export',
 }
 
 module.exports = nextConfig
