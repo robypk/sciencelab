391dc0b6473e4aaf42a895d0e731df559946133a
ROBY P K <robypk93@gmail.com>
2023-12-19 22:42:19 +0530

    checking

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    trailingSlash: true,
     output: 'export',
-    reactStrictMode: true,
-
- // Add basePath
-  basePath: '/github-pages',
-
     }
     module.exports = nextConfig
