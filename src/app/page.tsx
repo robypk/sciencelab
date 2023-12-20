1328e85ff161f933efcdb6baa97439dceb0264c3
ROBY P K <robypk93@gmail.com>
2023-12-20 18:10:28 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import Physics from "@/Components/Physics";
 export default function Home() {
   return (
     <div>
+      <div className="flex items-start justify-center font-bold text-5xl text-lime-500 shadow-black shadow-xl">
+        Roby
+      </div>
       <Banner />
       <div className=" p-14">
         <Physics />
