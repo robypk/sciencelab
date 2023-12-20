f10ce3f40ad25484e1f4a5b99e7ebf69251c9e52
ROBY P K <robypk93@gmail.com>
2023-12-21 02:11:14 +0530

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,6 @@ import CustomButton from "@/app/CustomButton";
 export default function Home() {
   return (
     <div>
-      <div className="flex items-start justify-center font-bold text-5xl text-lime-500 shadow-black shadow-xl">
-        Roby
-        <CustomButton ButtonName="Robyy" />
-      </div>
       <Banner />
       <div className=" p-14">
         <Physics />
