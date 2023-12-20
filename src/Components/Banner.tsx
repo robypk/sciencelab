b991c4c09c9c01051b5b340f635df19786f8380b
ROBY P K <robypk93@gmail.com>
2023-12-20 18:19:20 +0530

    pics added

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -11,7 +11,7 @@ const Banner: React.FC<BannerProps> = ({}) => {
       <div
         className=" relative bg-cover bg-center h-72"
         style={{
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url("/images/LabBanner.jpg")`,
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.9)), url("/LabBanner.jpg")`,
         }}
       >
         <div className="absolute bottom-10 left-10 text-5xl text-orange-300">
--- src/Components/Physics.tsx
+++ src/Components/Physics.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import potentiometer from "../images/potentiometer.jpg";
 import Card from "./Card";
 
 interface PhysicsProps {
@@ -13,49 +12,49 @@ const Physics: React.FC<PhysicsProps> = ({}) => {
       <div className="flex flex-wrap justify-start">
         <div>
           <Card
-            imageUrl="../images/potentiometer.jpg"
+            imageUrl="/potentiometer.jpg"
             title="PotentioMeter"
             ButtonName="To Start"
           />
         </div>
         <div>
           <Card
-            imageUrl={"../images/potentiometer.jpg"}
+            imageUrl={"/potentiometer.jpg"}
             title="Convex Lens"
             ButtonName="To Start"
           />
         </div>
         <div>
           <Card
-            imageUrl={"../images/potentiometer.jpg"}
+            imageUrl={"/potentiometer.jpg"}
             title="VoltMeter"
             ButtonName="To Start"
           />
         </div>
         <div>
           <Card
-            imageUrl={"../images/potentiometer.jpg"}
+            imageUrl={"/potentiometer.jpg"}
             title="VoltMeter"
             ButtonName="To Start"
           />
         </div>
         <div>
           <Card
-            imageUrl={"../images/potentiometer.jpg"}
+            imageUrl={"/potentiometer.jpg"}
             title="VoltMeter"
             ButtonName="To Start"
           />
         </div>
         <div>
           <Card
-            imageUrl={"../images/potentiometer.jpg"}
+            imageUrl={"/potentiometer.jpg"}
             title="VoltMeter"
             ButtonName="To Start"
           />
         </div>
         <div>
           <Card
-            imageUrl={"../images/potentiometer.jpg"}
+            imageUrl={"/potentiometer.jpg"}
             title="VoltMeter"
             ButtonName="To Start"
           />
