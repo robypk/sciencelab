29efcda10574a2cffa11e1f1ac31c402d3c3e9a8
ROBY P K <robypk93@gmail.com>
2023-12-22 02:37:09 +0530

    unity added

---
+++ src/app/UnityBuild.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+interface UnityBuildProps {
+  // props definition
+}
+
+const UnityBuild: React.FC<UnityBuildProps> = ({}) => {
+  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+    loaderUrl: "Build/MuseumWeb.loader.js",
+    dataUrl: "Build/MuseumWeb.data",
+    frameworkUrl: "Build/MuseumWeb.framework.js",
+    codeUrl: "Build/MuseumWeb.wasm",
+  });
+  return (
+    <div>
+      {!isLoaded && (
+        <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
+      )}
+      <Unity
+        unityProvider={unityProvider}
+        style={{ visibility: isLoaded ? "visible" : "hidden" }}
+      />
+    </div>
+  );
+};
+
+export default UnityBuild;
