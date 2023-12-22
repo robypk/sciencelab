821a1f7693bf36a91cde1de6e3dbd38b2ed7186a
ROBY P K <robypk93@gmail.com>
2023-12-23 00:17:07 +0530

    button logic added

--- src/app/CustomButton.tsx
+++ src/app/CustomButton.tsx
@@ -4,12 +4,27 @@ import "./globals.css";
 interface CustomButtonProps {
   // props definition
   ButtonName: string;
+  onClick?: () => void;
+  intaractable?: boolean;
 }
 
-const CustomButton: React.FC<CustomButtonProps> = ({ ButtonName }) => {
+const CustomButton: React.FC<CustomButtonProps> = ({
+  ButtonName,
+  onClick,
+  intaractable,
+}) => {
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
   return (
     <div>
-      <button className=" bg-inherit border-2 border-cyan-600 hover:bg-teal-400 text-white font-semibold py-2 px-4 rounded-full transition duration-300">
+      <button
+        className=" bg-inherit border-2 border-cyan-600 hover:bg-teal-400 text-white font-semibold py-2 px-4 rounded-full transition duration-300"
+        onClick={handleClick}
+        disabled={intaractable}
+      >
         {ButtonName}
       </button>
     </div>
