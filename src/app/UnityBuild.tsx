8e610a23f1246056b457c00f17077acabf5486cf
ROBY P K <robypk93@gmail.com>
2023-12-30 14:32:38 +0530

    compressed

--- src/app/UnityBuild.tsx
+++ src/app/UnityBuild.tsx
@@ -30,9 +30,9 @@ const UnityBuild: React.FC<UnityBuildProps> = ({
     removeEventListener,
   } = useUnityContext({
     loaderUrl: "unity/Build/unity.loader.js",
-    dataUrl: "unity/Build/unity.data",
-    frameworkUrl: "unity/Build/unity.framework.js",
-    codeUrl: "unity/Build/unity.wasm",
+    dataUrl: "unity/Build/unity.data.unityweb",
+    frameworkUrl: "unity/Build/unity.framework.js.unityweb",
+    codeUrl: "unity/Build/unity.wasm.unityweb",
   });
 
   /**
