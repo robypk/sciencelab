892523963bd65e8f247f0dcc68b6f85d1c7bfd4f
ajinpjohn <ajinpjohn.fx@gmail.com>
2024-03-16 22:47:45 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Roby Science Lab",
+  title: "Apparatus",
   description: "ScienceLab",
 };
 
