71bdd1721d9571bba0f08703e434bb920c8c5d9e
ajinpjohn <ajinpjohn.fx@gmail.com>
2024-04-29 02:44:11 +0530

    PT update

--- src/app/Physics.tsx
+++ src/app/Physics.tsx
@@ -58,7 +58,7 @@ const Physics: React.FC<PhysicsProps> = ({}) => {
         <div>
           <Card
             imageUrl={"PlaneTable_3PointProblem.jpg"}
-            title="Plane Table: 3-Point Problem "
+            title="Plane Table: Three Point Problem "
             UnityBuildName="PlaneTable_3PointProblem"
             SceneIndex={1}
           />
