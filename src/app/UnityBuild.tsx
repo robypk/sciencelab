fd93adbac423f67f8d178ec19950ee7009fdf280
ROBY P K <100525621+robypk@users.noreply.github.com>
2024-05-10 02:49:58 +0530

    Dynamic Pixel Ratio

--- src/app/UnityBuild.tsx
+++ src/app/UnityBuild.tsx
@@ -72,6 +72,35 @@ const UnityBuild: React.FC<UnityBuildProps> = ({
       removeEventListener("unityReady", unityisReady);
     };
   }, [unityisReady]);
+
+  // We'll use a state to store the device pixel ratio.
+  const [devicePixelRatio, setDevicePixelRatio] = useState(
+    window.devicePixelRatio
+  );
+
+  useEffect(
+    function () {
+      // A function which will update the device pixel ratio of the Unity
+      // Application to match the device pixel ratio of the browser.
+      const updateDevicePixelRatio = function () {
+        setDevicePixelRatio(window.devicePixelRatio);
+      };
+      // A media matcher which watches for changes in the device pixel ratio.
+      const mediaMatcher = window.matchMedia(
+        `screen and (resolution: ${devicePixelRatio}dppx)`
+      );
+      // Adding an event listener to the media matcher which will update the
+      // device pixel ratio of the Unity Application when the device pixel
+      // ratio changes.
+      mediaMatcher.addEventListener("change", updateDevicePixelRatio);
+      return function () {
+        // Removing the event listener when the component unmounts.
+        mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
+      };
+    },
+    [devicePixelRatio]
+  );
+
   /**
    *
    *
@@ -83,7 +112,7 @@ const UnityBuild: React.FC<UnityBuildProps> = ({
     <>
       <Unity
         unityProvider={unityProvider}
-        devicePixelRatio={window.devicePixelRatio}
+        devicePixelRatio={devicePixelRatio}
         className=" w-1"
       />
     </>
