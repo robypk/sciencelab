3dd677ff88a243021d17061ada3befe3545355e7
ROBY P K <100525621+robypk@users.noreply.github.com>
2024-05-16 14:10:19 +0530

    Update UnityBuild.tsx

--- src/app/UnityBuild.tsx
+++ src/app/UnityBuild.tsx
@@ -112,7 +112,7 @@ const UnityBuild: React.FC<UnityBuildProps> = ({
     <>
       <Unity
         unityProvider={unityProvider}
-        devicePixelRatio={1.01}
+        devicePixelRatio={1.08}
         className=" w-1"
       />
     </>
