09b89ba5f394e6e9b8f6854acdc79e79407693ea
ROBY P K <100525621+robypk@users.noreply.github.com>
2024-05-18 03:28:33 +0530

    Update Scene.tsx

--- src/app/virtuallab/Scene.tsx
+++ src/app/virtuallab/Scene.tsx
@@ -3,27 +3,10 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import * as THREE from "three";
 
-interface GLTFResult {
-  nodes: {
-    Level?: THREE.Mesh;
-    Sudo?: THREE.Mesh;
-    SudoHead?: THREE.Mesh;
-    Camera?: THREE.Mesh;
-    Camera_1?: THREE.Mesh;
-    Cactus?: THREE.Mesh;
-  };
-  materials: {
-    Level?: THREE.Material;
-    Sudo?: THREE.Material;
-    SudoHead?: THREE.Material;
-    Camera?: THREE.Material;
-    Lens?: THREE.Material;
-    Cactus?: THREE.Material;
-  };
-}
+interface GLTFResult {}
 
 function Model() {
-  const gltf = useGLTF("/Planetablegltftest.glb");
+  const gltf = useGLTF("Planetablegltftest.glb");
 
   // Debugging output to check if the GLTF is loaded correctly
   console.log(gltf);
